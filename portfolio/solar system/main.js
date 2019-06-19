dafed3397ba14392d55b144d0441f86b8a1e9ecb
Ivan Hsu <tanoshii06@gmail.com>
2019-06-19 19:57:10 +0800

    add Random Frequency

--- portfolio/solar system/main.js
+++ portfolio/solar system/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 	
+	var frequency = ['slow','medium','fast','furious'];
+	
+	function generateFreq(min,max){
+		
+		var freq = Math.floor(Math.random()*max-min);
+		
+		return freq;
+		
+	}
+	
+	for (var i =1; i<=11; i++){
+		
+		var freq = frequency[generateFreq(0,4)];
+		
+		$('.space div').eq(i).toggleClass(freq);
+		
+	}
+	
 	$('.voyager').click(function(e){
 		$(this).toggleClass('reverse');
 		console.log('ha');

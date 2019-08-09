97fb0111e7548e7c0286c153b40496521dbc6f70
Ivan Hsu <tanoshii06@gmail.com>
2019-08-09 17:14:26 +0800

    js modified my wcc

--- portfolio/latest article/main.js
+++ portfolio/latest article/main.js
@@ -1,58 +1,37 @@
 var clicks = 0;
-$(document).ready(function(e){
-	
-	//arrange cards
-	for(var i = 0; i<6; i++){
-		
-		var t = 374-14*i;
-		var w = 280-10*i;
-		var z = -1*i;
-		
-		$('#card-set li').eq(i).find('.card').css({'top':t +'px','background-color':'','width': w + 'px','z-index':5 + z});
-		
+$(document).ready(function (e) {
+
+	// arrange cards
+	for (var i = 0; i < 6; i++) {
+		var t = 374 - 14 * i;
+		var w = 280 - 10 * i;
+		var z = -1 * i;
+
+		$('#card-set li').eq(i).find('.card').css({ 'top': t + 'px', 'background-color': '', 'width': w + 'px', 'z-index': 5 + z });
+
 	};
-	
-	//next card
-	var currentStatus = '';
-	
-	$('.next').click(function(e){
+
+	// next card
+	$('.next').click(function (e) {
 		event.preventDefault();
-		
-		if(clicks > 4){
-			clicks = 4;
+
+		if (clicks >= 5) {
+			clicks = 5;
+		} else {
 			$('#card-set li').eq(clicks).find('.card').fadeOut(400);
-		} else {
-			if (currentStatus == 'prev'){
-				$('#card-set li').eq(clicks +1).find('.card').fadeOut(400);
-			} else{
-				$('#card-set li').eq(clicks).find('.card').fadeOut(400);
-				clicks = clicks +1;
-			}
+			clicks++;
 		}
-		currentStatus = 'next';
-		console.log('next' + clicks)
-
 	});
 
-
-	//previous card
-	$('.prev').click(function(e){
+	// previous card
+	$('.prev').click(function (e) {
 		event.preventDefault();
-		
-		if(clicks <= 0){
-			clicks = 0;
-			$('#card-set li').eq(clicks).find('.card').fadeIn(400);
-		} else {
-			if (currentStatus == 'next'){
-				$('#card-set li').eq(clicks -1).find('.card').fadeIn(400);
-			} else {
-				$('#card-set li').eq(clicks).find('.card').fadeIn(400);
-				clicks = clicks -1;
-			} 
-		}
-		currentStatus = 'prev';
-		console.log('prev' + clicks)
 
+		if (clicks <= 0) {
+			clicks = 0;
+		} else {
+			$('#card-set li').eq(clicks - 1).find('.card').fadeIn(400);
+			clicks--;
+		}
 	});
-	
 });

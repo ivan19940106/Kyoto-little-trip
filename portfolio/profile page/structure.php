5c84b61963aa83d0d8fcb3e308f8552c36bf3269
Ivan Hsu <tanoshii06@gmail.com>
2019-11-20 14:48:42 +0800

    profile as assignment2

---
+++ portfolio/profile page/structure.php
@@ -0,0 +1,25 @@
+  
+    <div class="header">
+        <ul class="nav clearfix">
+            <li class="logo"><a href="#"><img src="images/logo.png" alt="logo" width="100"></a></li>
+            <li class="about"><a href="#">about</a></li>
+            <li class="contact"><a href="contact.html">contact</a></li>
+            <ul>
+                <li class="social-media">social media</li>
+                <li class="fb"><a href="https://www.facebook.com/profile.php?id=100000301162192" target="_blank">facebook</a></li>
+                <li class="ig"><a href="https://www.instagram.com/ivan19940106/" target="_blank">instagram</a></li>
+            </ul>
+        </ul>
+    </div>
+
+    <div class="banner">
+        
+    </div>
+
+    <div class="container clearfix">
+        
+    </div>
+
+    <div class="footer">
+        <div class="copyright">© all rights reserved</div>
+    </div>

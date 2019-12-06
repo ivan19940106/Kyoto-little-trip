6ed39e17e495ba6afd2415356f5e965816ecfa33
Ivan Hsu <tanoshii06@gmail.com>
2019-12-06 15:53:27 +0800

    add card set layout
    changed
    model.css .models & .models-card
    model.html js crc
    main.js

--- portfolio/atelier koco./js/main.js
+++ portfolio/atelier koco./js/main.js
@@ -1,5 +1,6 @@
 var clicks = 0;
 var clicksAtelier = 0;
+var swipe = 0;
 $(document).ready(function(e){
     // gallery-modal
 
@@ -197,5 +198,40 @@ $(document).ready(function(e){
             $(".nav").slideUp();
         } 
     });
+
+    //models-card
+    if($(window).width() <= 768){
+        //cards length
+        var len = $(".models").find('div.models-card').length;
+        console.log(len);
+
+        // arrange cards
+        for (var i = 0; i < len; i++) {
+            var top = 100-14*i;
+            var width = 337-10*i;
+            var zIndex = len-i;
+            $('.models').find('div.models-card').eq(i).css({ 'top': top + 'px', 'width': width + 'px', 'z-index': zIndex });
+        };
+
+        // next card
+        $("div.models-card").on("swiperight",function(e){
+            if (swipe >= len) {
+                swipe = len;
+            } else {
+                $('.models').find('div.models-card').eq(swipe).fadeOut(400);
+                swipe++;
+            }
+        });
+
+        // previous card
+        $("div.models-card").on("swipeleft",function(e){
+            if (swipe <= 0) {
+                swipe = 0;
+            } else {
+                $('.models').find('div.models-card').eq(swipe - 1).fadeIn(400);
+                swipe--;
+            }
+        });
+    }
 
 });

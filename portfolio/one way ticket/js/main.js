f356339c83280b5e25a4fd043c7a078f7383a1ac
Ivan Hsu <tanoshii06@gmail.com>
2019-12-13 22:43:25 +0800

    add one way ticket to my portfolio

---
+++ portfolio/one way ticket/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(e){
+    var vm = new Vue({
+        el: "#app",
+        data: {
+            company: "台灣鐵路局",
+            ticket: {
+                date: "2020.01.10-01.11",
+                from: "臺北",
+                to: "高雄",
+                direction: "south",
+                other: "<b>限當日當次車有效</b>",
+                price: 700,
+                discount: 0.5,
+                link: "https://www.railway.gov.tw/tra-tip-web/tip"
+            }
+        }
+    });
+});

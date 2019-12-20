135ae54bddc3eed9867fd72885ef5e430a9a7de5
Ivan Hsu <tanoshii06@gmail.com>
2019-12-20 11:11:56 +0800

    fixed coffee-cup and mug handle

--- portfolio/bubble tea/js/main.js
+++ portfolio/bubble tea/js/main.js
@@ -5,10 +5,10 @@ $(document).ready(function(e){
             editing: true,
             features: ['店長推薦', '國外進口', '本土好茶', '冬季限定'],
             milkTeas: [
-                {name: "四季奶青加珍珠", foam: 10, milk: 30, 'green-tea': 20, 'black-tea': 0, bubbles: 2, type: 'thermos', accType: 'top'},
-                {name: "奶蓋南非紅茶加珍珠", foam: 5, milk: 10, 'green-tea': 0, 'black-tea': 20, bubbles: 8, type: 'paper-cup', accType: 'straw'},
-                {name: "珍珠奶綠", foam: 10, milk: 20, 'green-tea': 30, 'black-tea': 0, bubbles: 5, type: 'coffee-cup'},
-                {name: "珍珠紅茶拿鐵", foam: 5, milk: 30, 'green-tea': 0, 'black-tea': 20, bubbles: 3, type: 'mug'}
+                {name: "四季奶青加珍珠", foam: 10, milk: 30, 'green-tea': 20, 'black-tea': 0, bubbles: 5, type: 'thermos', accType: 'top'},
+                {name: "京都風雲珍珠奶綠", foam: 5, milk: 10, 'green-tea': 20, 'black-tea': 0, bubbles: 10, type: 'paper-cup', accType: 'straw'},
+                {name: "日月潭奶蓋紅茶", foam: 5, milk: 15, 'green-tea': 0, 'black-tea': 0, bubbles: 1, type: 'coffee-cup'},
+                {name: "暖心紅茶拿鐵", foam: 5, milk: 25, 'green-tea': 0, 'black-tea': 10, bubbles: 5, type: 'mug'}
             ],
             cupTypes: ['thermos', 'paper-cup', 'coffee-cup', 'mug'],
             cupTypesChinese: ['自備環保保溫杯', '風味手搖杯', '古典白瓷咖啡杯', '經典馬克杯'],

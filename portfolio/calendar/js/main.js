22e23a5ca9a02836a98eabb5208ada45f03075dc
Ivan Hsu <tanoshii06@gmail.com>
2020-01-28 01:55:16 +0800

    calendar computed & back in time

--- portfolio/calendar/js/main.js
+++ portfolio/calendar/js/main.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
     var vm = new Vue({
         el: '#app',
         data: {
+            dateToday: new Date(),
             banner: ['財源廣進'],
             pages: 10,
             year: [2019,2020],
-            yearIndex: [['甲','子'],['乙','丑'],['丙','寅'],['丁','卯'],['戊','辰'],['己','巳'],['庚','午'],['辛','未'],['壬','申'],['癸','酉'],['甲','戌'],['乙','亥']],
+            yearIndexX: ['甲','乙','丙','丁','戊','己','庚','辛','壬','癸'],
+            yearIndexY: ['子','醜','寅','卯','辰','巳','午','未','申','酉','戌','亥'],
             solarTerms: ['立春','雨水','驚蟄','春分','清明','穀雨','立夏','小滿','芒種','夏至','小暑','大暑','立秋','處暑','白露','秋分','寒露','霜降','立冬','小雪','大雪','冬至','小寒','大寒'],
-            dateTraditional: ['初一'],
+            dateTraditional: ['初一','初二','初三','初四','初五','初六','初七','初八','初九','初十','十一','十二','十三','十四','十五','十六','十七','十八','十九','二十','廿一','廿二','廿三','廿四','廿五','廿六','廿七','廿八','廿九','三十','三一'],
             weatherText: ['晴','雨','雲'],
             weatherImagePath: ["./images/sun.png", "./images/umbrella.png", "./images/cloud.png"],
             date: [1],
@@ -19,20 +21,76 @@ $(document).ready(function(){
             quote: ["以銅為鏡，可以正衣冠。以古為鏡，可以知興替。以人為鏡，可以明得失。"]
         },
         methods: {
-            randomWeatherText: function(){
-                return vm.weatherText[Math.floor(Math.random()*3)];
+            next: function(){
+                
             },
-            randomWeatherImagePath: function(){
-                var path = "";
-                var text = $('.weather span').text();
-                if(text.includes('晴')){
-                    path = vm.weatherImagePath[0];
-                } else if(text.includes('雨')) {
-                    path = vm.weatherImagePath[1];
-                } else if(text.includes('雲')){
-                    path = vm.weatherImagePath[2];
+            prev: function(){
+
+            }
+        },
+        computed: {
+            yearToday: function(){
+                var year = this.dateToday.getFullYear()-1911;
+                if(year > 0 && year<10){
+                    year = `0${year}`
+                } else if(year<0){
+                    year = `民國負${year.toString().replace('-','')}`
+                } else if(year == 0){
+                    year = '民國元';
                 }
-                return path;
+                return year;
+            },
+            yearIndexToday: function(){
+                var x = 0;
+                var y = 0;
+                var year = this.dateToday.getFullYear();
+                if(year%10-3 > 0){
+                    x = year%10-3-1;
+                } else if(year%10-3 < 0){
+                    x = year%10-3+10-1;
+                } else if(year%10-3 == 0){
+                    x = 9;
+                }
+                if(year%12-3 > 0){
+                    y = year%12-3-1;
+                } else if(year%12-3 < 0){
+                    y = year%12-3+12-1;
+                } else if(year%12-3 == 0){
+                    y = 11;
+                }
+                return this.yearIndexX[x]+this.yearIndexY[y];
+            },
+            //based on 2020: http://ihouse.hkedcity.net/~hm1203/atmosphere/climate-china-24.htm
+            solarTermsToday: function(){
+                if(this.dateToday.getDate()==1){
+                    return this.solarTerms[this.dateToday.getMonth()];
+                } else if(this.dateToday.getDate()==15){
+                    return this.solarTerms[this.dateToday.getMonth()+1];
+                } else {
+                    return null;
+                }
+            },
+            dateTraditionalToday: function(){
+                return this.dateTraditional[this.dateToday.getDate()-1];
+            },
+            dateOnToday: function(){
+                var date = this.dateToday.getDate();
+                if(date<10){
+                    date = `0${date}`;
+                }
+                return date;
+            },
+            monthToday: function(){
+                var month = this.dateToday.getMonth()+1;
+                return month;
+            },
+            monthEnglishToday: function(){
+                var monthEnglish = this.monthEnglish[this.monthToday-1];
+                return monthEnglish;
+            },
+            weekdayToday: function(){
+                var weekday = this.dateToday.getDay();
+                return this.weekday[weekday-1];
             }
         }
     });
@@ -67,7 +125,7 @@ $(document).ready(function(){
         }
         //controls
         $('.controls').css({
-            top: windowHeight/2+pageHeight/2+50+'px'
+            top: windowHeight/2+pageHeight/2+90+'px'
         });
     }
 
@@ -111,6 +169,13 @@ $(document).ready(function(){
             clicks --;
             //move backward
         }
+    });
+
+    //go back in time
+    $('.controls a.go').click(function(e){
+        e.preventDefault();
+        var val = $('.backTime').val();
+        vm.dateToday = new Date(val);
     });
 
     //random weather with jQuery

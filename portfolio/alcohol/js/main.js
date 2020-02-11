6b452e21e2d7024ae411319d2a2c37658ef80b54
Ivan Hsu <tanoshii06@gmail.com>
2020-02-11 19:45:11 +0800

    add 75% alcohol to portfolio

---
+++ portfolio/alcohol/js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            alcVol: 200,
+            watVol: 60
+        },
+        computed: {
+            calcAlcCon: function(){
+                var alcCon = (this.alcVol*0.95/(this.alcVol+this.watVol)*100).toFixed(1);
+                return alcCon;
+            }
+        }
+    });
+    window.vm = vm;
+
+    //smoke.js
+    if(window.innerWidth>=768){
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        canvas.width = 600;
+        canvas.height = 600;
+    
+        var party = SmokeMachine(ctx, [240, 240, 240]);
+        party.start(); // start animating
+        party.addSmoke(250,330,10); // wow we made smoke
+    
+        setInterval(function(){
+            party.stop(); // stop animating
+            party.addSmoke(290,450,50);
+            party.addSmoke(250,330,20);
+            for(var i=0;i<10;i++){
+                party.step(10); // pretend 10 ms pass and rerender
+            }
+    
+            setInterval(function(){
+                party.start();
+            },1000);
+    
+        },4000);
+    } else {
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        canvas.width = window.innerWidth;
+        canvas.height = 500;
+    
+        var party = SmokeMachine(ctx, [240, 240, 240]);
+        party.start(); // start animating
+        party.addSmoke(170,330,10); // wow we made smoke
+    
+        setInterval(function(){
+            party.stop(); // stop animating
+            party.addSmoke(160,350,30);
+            party.addSmoke(150,230,20);
+            for(var i=0;i<10;i++){
+                party.step(10); // pretend 10 ms pass and rerender
+            }
+    
+            setInterval(function(){
+                party.start();
+            },1000);
+    
+        },4000);
+    }
+});

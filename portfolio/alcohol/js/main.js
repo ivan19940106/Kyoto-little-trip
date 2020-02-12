5ac5ec85eaafb9d6f34a3b19a4cb9f1fc478673f
Ivan Hsu <tanoshii06@gmail.com>
2020-02-12 21:26:38 +0800

    alcohol spray modified

--- portfolio/alcohol/js/main.js
+++ portfolio/alcohol/js/main.js
@@ -19,13 +19,17 @@ $(document).ready(function(){
         var canvas = document.getElementById('canvas');
         var ctx = canvas.getContext('2d');
         canvas.width = 600;
-        canvas.height = 600;
+        canvas.height = 500;
     
-        var party = SmokeMachine(ctx, [240, 240, 240]);
+        var r = Math.random()*255;
+        var g = Math.random()*255;
+        var b = Math.random()*255;
+
+        var party = SmokeMachine(ctx, [r, g, b]);
         party.start(); // start animating
         party.addSmoke(250,330,10); // wow we made smoke
-    
-        setInterval(function(){
+
+        setTimeout(function(){
             party.stop(); // stop animating
             party.addSmoke(290,450,50);
             party.addSmoke(250,330,20);
@@ -33,22 +37,39 @@ $(document).ready(function(){
                 party.step(10); // pretend 10 ms pass and rerender
             }
     
-            setInterval(function(){
+            setTimeout(function(){
                 party.start();
             },1000);
     
-        },4000);
+        },2000);
+    
+        $('.top').on('mouseover click', function(){
+            party.stop(); // stop animating
+            party.addSmoke(290,450,50);
+            party.addSmoke(250,330,20);
+            for(var i=0;i<10;i++){
+                party.step(10); // pretend 10 ms pass and rerender
+            }
+    
+            setTimeout(function(){
+                party.start();
+            },1000);
+        });
     } else {
         var canvas = document.getElementById('canvas');
         var ctx = canvas.getContext('2d');
         canvas.width = window.innerWidth;
         canvas.height = 500;
+
+        var r = Math.random()*255;
+        var g = Math.random()*255;
+        var b = Math.random()*255;
     
-        var party = SmokeMachine(ctx, [240, 240, 240]);
+        var party = SmokeMachine(ctx, [r, g, b]);
         party.start(); // start animating
         party.addSmoke(170,330,10); // wow we made smoke
-    
-        setInterval(function(){
+
+        setTimeout(function(){
             party.stop(); // stop animating
             party.addSmoke(160,350,30);
             party.addSmoke(150,230,20);
@@ -56,10 +77,23 @@ $(document).ready(function(){
                 party.step(10); // pretend 10 ms pass and rerender
             }
     
-            setInterval(function(){
+            setTimeout(function(){
                 party.start();
             },1000);
     
-        },4000);
+        },2000);
+    
+        $('.top').on('click', function(){
+            party.stop(); // stop animating
+            party.addSmoke(160,350,30);
+            party.addSmoke(150,230,20);
+            for(var i=0;i<10;i++){
+                party.step(10); // pretend 10 ms pass and rerender
+            }
+    
+            setTimeout(function(){
+                party.start();
+            },1000);
+        });
     }
 });

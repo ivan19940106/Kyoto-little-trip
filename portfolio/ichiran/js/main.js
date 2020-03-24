700ff12061c84f0980ef18a2d60819cc4982964b
Chang-ko Hsu <tanoshii06@gmail.com>
2020-03-24 13:43:06 +0800

    add menu to ichiran

--- portfolio/ichiran/js/main.js
+++ portfolio/ichiran/js/main.js
@@ -3,13 +3,46 @@ $(document).ready(function(){
         el: '#app',
         data: {
             negis: 100,
-            meats: 2
+            meats: 2,
+            kara: 70,
+            ordered: false
         },
         computed: {}
     });
     window.vm = vm;
 
+    $('#ordered').on('change', function(){
+        if($('#ordered').val()){
+            vm.ordered = true;
+        }
+    });
+
+    $('.negi-row label').click(function(){
+        $('label').removeClass('circled');
+        $(this).addClass('circled');
+        vm.negis = parseInt($(`#${$(this).attr('for')}`).val());
+    });
+    $('.meat-row label').click(function(){
+        $('label').removeClass('circled');
+        $(this).addClass('circled');
+        vm.meats = parseInt($(`#${$(this).attr('for')}`).val());
+    });
+    $('.kara-row label').click(function(){
+        $('label').removeClass('circled');
+        $(this).addClass('circled');
+        vm.kara = parseInt($(`#${$(this).attr('for')}`).val());
+    });
+
+    $('.kaedama').click(function(){
+        if($('.ingradients').hasClass('eaten-up')){
+            vm.negis = 0;
+            vm.meats = 0;
+            vm.kara = 0;
+            $('.ingradients').removeClass('eaten-up');
+        }
+    });
+
     $('.bowl').click(function(){
-        $('.ingradients').toggleClass('eaten-up');
+        $('.ingradients').addClass('eaten-up');
     });
 });

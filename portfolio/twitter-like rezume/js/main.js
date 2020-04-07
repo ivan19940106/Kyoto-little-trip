9001a8510d8094623bfe8bdca85a37959ef62666
Chang-ko Hsu <tanoshii06@gmail.com>
2020-04-08 03:46:41 +0800

    add twitter-like rezume to portfolio

---
+++ portfolio/twitter-like rezume/js/main.js
@@ -0,0 +1,35 @@
+window.addEventListener('load',function(){
+    //cover photo minesweeper
+    Vue.component('minesweeper', {
+        template: '<css-doodle class="minesweeper" use="var(--rule)" @click="refresh"></css-doodle>',
+        methods: {
+            refresh: function() {
+                const doodle = document.querySelector('css-doodle');
+                doodle.update();
+            }
+        }
+    });
+    //Vue.js
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            darkMode: 'on'
+        },
+        methods: {
+            changeDarkMode(){
+                var self = this;
+                $('#dark-mode').toggleClass('off');
+                if($('#dark-mode').hasClass('off')){
+                    $('body').addClass('normal-background');
+                    self.darkMode = 'off';
+                } else {
+                    $('body').removeClass('normal-background');
+                    self.darkMode = 'on';
+                }
+            }
+        }
+    });
+    window.vm = vm;
+    //t.js typing effect
+    // $('.description').t();
+});
